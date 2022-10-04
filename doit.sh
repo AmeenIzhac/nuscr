ad116e77a75633a871542f8dc0700b65284b632b
ameen <ameen.izhac@gmail.com>
2022-10-04 20:38:16 +0100

    graceful failure complete

--- doit.sh
+++ doit.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-time dune exec nuscr -- --local-graceful-failure=$1 examples/presentationExamples/original/$1.scr
+time dune exec nuscr -- --graceful-failure=$1 examples/presentationExamples/original/$1.scr
